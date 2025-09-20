a3a9bb5adfdec4bb58fd72ddd236f978797b6830
Atul Sharma <atulsharma02004@gmail.com>
2025-09-21 02:26:50 +0530

    Fixed price toggle button

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,42 +1,116 @@
-import { TextField } from '@mui/material'
+import { Checkbox, FormControlLabel, Radio, RadioGroup, TextField, ToggleButton } from '@mui/material'
+import { CheckIcon, CrossIcon, LockIcon, LockOpen } from 'lucide-react'
 import React, { useState } from 'react'
+import { IoAddCircle } from 'react-icons/io5'
+import ToggleSwitch from './ToggleSwich'
 
 const SideBar = () => {
     const [pickup,setPickup] = useState('')
+    const [destination, setDestination] = useState('')
+    const [isOn, setIsOn ] = useState(false)
   return (
-    <section className='w-[25%] bg-red-100 h-screen overflow-y-auto space-y-4'>
+    <section className='w-[25%] overflow-y-scroll-scroll'>
 
-        <form className='px-5 py-6'>
+        <form>
 
-            <h3 className='text-sm mb-4'> Step 1 of 4 <b>Booking details</b></h3>
+            <div className='px-5 py-6'>
+                <h3 className='text-sm mb-4'> Step 1 of 4 <b>Booking details</b></h3>
 
-            <div className='mb-4'>
-                <TextField
-                label="Add pickup(required)"      // 👈 Floating label
-                variant="outlined"     // outlined | filled | standard
-                fullWidth
-                required
-                placeholder='Add your pickup location'
-                value={pickup}
-                onChange={(e) => setPickup(e.target.value)}
-                />
+                <div className='mb-4'>
+                    <TextField
+                    label="Add pickup(required)"      // 👈 Floating label
+                    variant="outlined"     // outlined | filled | standard
+                    fullWidth
+                    required
+                    placeholder='Add your pickup location'
+                    value={pickup}
+                    onChange={(e) => setPickup(e.target.value)}
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <TextField
+                    label="Add destination(required)"      // 👈 Floating label
+                    variant="outlined"     // outlined | filled | standard
+                    fullWidth
+                    required
+                    placeholder='Add your destination'
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
+                    />
+                </div>
+
+                <div 
+                className='flex gap-3 items-center align-center'>
+                    <IoAddCircle size={20}/> 
+                    <span className='inline-block'>Add Destination</span>
+                </div>
             </div>
 
-            <div className='mb-4'>
-                <TextField
-                label="Add destination(required)"      // 👈 Floating label
-                variant="outlined"     // outlined | filled | standard
-                fullWidth
-                required
-                placeholder='Add your pickup location'
-                value={pickup}
-                onChange={(e) => setPickup(e.target.value)}
-                />
+            <div className='flex gap-4 px-4 items-center justify-center'>
+                
+                <RadioGroup row defaultChecked="now">
+                    <FormControlLabel
+                    value="now"
+                    control={
+                        <Radio
+                        sx={{
+                            color: "green", // unchecked color
+                            "&.Mui-checked": {
+                            color: "green", // checked color
+                            },
+                            p: 1, // padding around the radio
+                        }}
+                        />
+                    }
+                    label="Book for now"
+                    />
+                
+
+                
+                    <FormControlLabel
+                    value="later"
+                    control={
+                        <Radio
+                        sx={{
+                            color: "green", // unchecked color
+                            "&.Mui-checked": {
+                            color: "green", // checked color
+                            },
+                            p: 1, // padding around the radio
+                        }}
+                        />
+                    }
+                    label="Book for later"
+                    />
+                
+
+                </RadioGroup>
+                    
+                
+
             </div>
 
-            
+            <div className='w-full bg-[#F8F6F2] px-4 py-5'>
+                <div className='flex items-center w-full justify-between'>
+                    <div className='flex gap-3 items-center'>
+                    <LockIcon color='#145389'/>
+                    <p className='text-xl text-[#145389] font-bold'>Fixed Price</p>
+                    </div>
 
-            
+                    <ToggleSwitch enabled={isOn} onToggle={setIsOn}/>
+
+
+
+                </div>
+
+                <p className='mt-2'>
+                    Lock in a price with no additional charges.
+                </p>
+
+                
+
+            </div>            
 
         </form>
 
---
+++ src/components/ToggleSwich.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function ToggleSwitch({enabled, onToggle}) {
+  
+
+  return (
+    <div
+      className={`flex items-center w-14 h-8 p-1 rounded-full cursor-pointer select-none transition-colors ${
+        enabled ? "bg-green-500" : "bg-red-500"
+      }`}
+      onClick={() => onToggle(!enabled)}
+    >
+      {/* Sliding circle */}
+      <div
+        className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
+          enabled ? "translate-x-6" : "translate-x-0"
+        }`}
+      />
+    </div>
+  );
+}
