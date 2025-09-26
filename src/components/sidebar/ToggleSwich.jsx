14c1298a3f42647c6d7247d81d1bcf3812947b20
Gajinder Singh <thakur.somu5704@gmail.com>
2025-09-26 23:49:24 +0530

    Toggle color change, added cross & tick icons

--- src/components/sidebar/ToggleSwich.jsx
+++ src/components/sidebar/ToggleSwich.jsx
@@ -1,19 +1,25 @@
-export default function ToggleSwitch({enabled, onToggle}) {
-  
+import { Check, X } from "lucide-react";
 
+export default function ToggleSwitch({ enabled, onToggle }) {
   return (
     <div
       className={`flex items-center w-14 h-8 p-1 rounded-full cursor-pointer select-none transition-colors ${
-        enabled ? "bg-green-500" : "bg-red-500"
+        enabled ? "bg-green-500" : "bg-gray-300"
       }`}
       onClick={() => onToggle(!enabled)}
     >
       {/* Sliding circle */}
       <div
-        className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
+        className={`flex items-center justify-center bg-white w-6 h-6 rounded-full shadow-md transform transition-transform ${
           enabled ? "translate-x-6" : "translate-x-0"
         }`}
-      />
+      >
+        {enabled ? (
+          <Check size={14} className="text-green-500" />
+        ) : (
+          <X size={14} className="text-gray-500" />
+        )}
+      </div>
     </div>
   );
 }
